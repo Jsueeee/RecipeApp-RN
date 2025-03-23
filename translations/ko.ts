f3c63fa2b1a7496409b2754460c954b6c0de50ae
Jsueeee <suyeon.aos@gmail.com>
2025-03-23 14:41:28 +0900

    chore: expo-localization 설치

---
+++ translations/ko.ts
@@ -0,0 +1,10 @@
+export default {
+  login: {
+    title: "레시피 저장소",
+    subtitle: "나만의 레시피를 저장하세요",
+    kakao: "카카오로 시작하기",
+    naver: "네이버로 시작하기",
+    google: "구글로 시작하기",
+  },
+  // 다른 섹션들...
+};
