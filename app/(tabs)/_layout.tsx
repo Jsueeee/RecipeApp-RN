dbe58f79e7b139f1f638f9599c2ce27da61a2a3f
Jsueeee <suyeon.aos@gmail.com>
2025-03-27 22:19:50 +0900

    feat: 바텀 네비게이션 뷰 및 탭 버튼 정의

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,147 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import FridgeIcon from "@/assets/images/ic_nav_fridge.svg";
+import MyPageIcon from "@/assets/images/ic_nav_my_page.svg";
+import RecipeIcon from "@/assets/images/ic_nav_recipe.svg";
+import SearchIcon from "@/assets/images/ic_nav_search.svg";
+import i18n from "@/lib/i18n";
+import * as Haptics from "expo-haptics";
+import { Tabs } from "expo-router";
+import React from "react";
+import {
+  Pressable,
+  PressableProps,
+  StyleProp,
+  StyleSheet,
+  Text,
+  ViewStyle,
+} from "react-native";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+const TabBarLabel = ({
+  focused,
+  label,
+}: {
+  focused: boolean;
+  label: string;
+}) => (
+  <Text
+    className={`text-bottomNavItem ${
+      focused ? "text-text-strong" : "text-text-assistive"
+    }`}
+  >
+    {i18n.t(label)}
+  </Text>
+);
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+const TabBarButton = ({
+  children,
+  style,
+  ...props
+}: {
+  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+} & Omit<PressableProps, "style">) => {
+  return (
+    <Pressable
+      {...props}
+      onPress={(e) => {
+        Haptics.selectionAsync();
+        props.onPress?.(e);
+      }}
+      style={[styles.tabBarButton, style]}
+    >
+      {children}
+    </Pressable>
+  );
+};
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+        tabBarButton: (props) => <TabBarButton {...props} />,
+        tabBarIconStyle: styles.tabBarIcon,
+        tabBarLabelStyle: styles.tabBarLabel,
+        tabBarActiveTintColor: "#4BD2B0",
+        tabBarInactiveTintColor: "#7F8A85",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          tabBarIcon: ({ color }) => (
+            <FridgeIcon color={color} width={20} height={20} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel focused={focused} label="bottom_tab.fridge" />
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="recipe"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <RecipeIcon color={color} width={20} height={20} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel focused={focused} label="bottom_tab.recipe" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <SearchIcon color={color} width={20} height={20} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel focused={focused} label="bottom_tab.search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="myPage"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MyPageIcon color={color} width={20} height={20} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel focused={focused} label="bottom_tab.myPage" />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+export const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: "white",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ECEFED",
+    paddingHorizontal: 16,
+    position: "absolute",
+    elevation: 0,
+    shadowOpacity: 0,
+    maxWidth: 500,
+    alignSelf: "center",
+  },
+  tabBarButton: {
+    flex: 1,
+    paddingVertical: 6,
+  },
+  tabBarIcon: {
+    width: 20,
+    height: 20,
+  },
+  tabBarLabel: {
+    fontSize: 10,
+    lineHeight: 18,
+    fontWeight: "600",
+    marginTop: 2,
+  },
+});
