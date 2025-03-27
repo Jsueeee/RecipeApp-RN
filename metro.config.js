be64456d33a5da265ff1a5e00f31049134981800
suyeon <66951780+Jsueeee@users.noreply.github.com>
2025-03-27 22:24:45 +0900

    feat: 바텀 탭 네비게이션 정의 (#5)
    * chore: svg 사용하기 위한 sdk 설치
    
    * chore: 바텀 탭 네비게이션 타이틀에 사용할 string 추가
    
    * chore: 바텀 탭 네비게이션 아이템에 사용할 svg 애셋 추가
    
    * chore: 햅틱 설치
    
    * chore: 검색 아이콘 svg currentColor 누락 부분 수정
    
    * chore: 햅틱 sdk 설치 파일 추가
    
    * chore: 마이페이지 바텀 라벨 값 수정
    
    * feat: 앞으로 사용할 탭 껍데기 추가
    
    * feat: 바텀 네비게이션 뷰 및 탭 버튼 정의

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,24 @@
+/* eslint-env node */
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
 
 module.exports = withNativeWind(config, { input: "./global.css" });
