35ed87b4433f133a144012a332ae43398b7757b6
Jsueeee <suyeon.aos@gmail.com>
2025-03-30 01:49:45 +0900

    chore: 탭 선택에 따른 스크롤 이동 구현

--- app/(tabs)/(fridge)/components/CategoryTabs.tsx
+++ app/(tabs)/(fridge)/components/CategoryTabs.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ScrollView, Pressable, Text } from "react-native";
+import React, { useRef } from "react";
+import { FlatList, Pressable, Text, ViewToken } from "react-native";
 import clsx from "clsx";
 
 interface Props {
@@ -13,32 +13,47 @@ export function CategoryTabs({
   selectedTabIndex,
   onSelectTabIndex,
 }: Props) {
-  return (
-    <ScrollView
-      horizontal
-      className="bg-gray-100"
-      showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{ paddingHorizontal: 16, paddingVertical: 12 }}
+  const flatListRef = useRef<FlatList>(null);
+
+  const handleTabPress = (index: number) => {
+    onSelectTabIndex(index);
+
+    flatListRef.current?.scrollToIndex({
+      index,
+      animated: true,
+      viewPosition: 0.5, // 선택된 항목을 중앙에 위치
+    });
+  };
+
+  const renderItem = ({ item, index }: { item: string; index: number }) => (
+    <Pressable
+      onPress={() => handleTabPress(index)}
+      className={clsx(
+        "px-3 py-[9px] rounded-full",
+        index === selectedTabIndex ? "bg-primary-strong" : "bg-gray-100"
+      )}
     >
-      {tabs.map((tab, index) => (
-        <Pressable
-          key={index}
-          onPress={() => onSelectTabIndex(index)}
-          className={`px-3 py-[9px] rounded-full ${
-            index === selectedTabIndex ? "bg-primary-strong" : "bg-gray-100"
-          }`}
-        >
-          <Text
-            className={clsx(
-              index === selectedTabIndex
-                ? ["text-utility3 text-white"]
-                : ["text-utility2 text-text-normal"]
-            )}
-          >
-            {tab}
-          </Text>
-        </Pressable>
-      ))}
-    </ScrollView>
+      <Text
+        className={clsx(
+          index === selectedTabIndex
+            ? "text-utility3 text-white"
+            : "text-utility2 text-text-normal"
+        )}
+      >
+        {item}
+      </Text>
+    </Pressable>
+  );
+
+  return (
+    <FlatList
+      ref={flatListRef}
+      data={tabs}
+      renderItem={renderItem}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="bg-gray-100"
+      contentContainerStyle={{ paddingHorizontal: 16, paddingVertical: 12 }}
+    />
   );
 }
