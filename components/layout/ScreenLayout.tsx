b549aba959338929d43e60f750b9120ef76171bc
Jsueeee <suyeon.aos@gmail.com>
2025-03-30 17:01:47 +0900

    feat: ScreenLayout 컴포넌트 추가

---
+++ components/layout/ScreenLayout.tsx
@@ -0,0 +1,37 @@
+import { Header } from "@/components/Header";
+import { SafeAreaView, View, ScrollView } from "react-native";
+import { ReactNode } from "react";
+import { router } from "expo-router";
+
+interface Props {
+  title: string;
+  children: ReactNode;
+  backgroundColor?: string;
+  footer?: ReactNode;
+  onBackClick?: () => void;
+}
+
+export function ScreenLayout({
+  title,
+  children,
+  backgroundColor = "white",
+  footer,
+  onBackClick,
+}: Props) {
+  return (
+    <SafeAreaView className={`flex-1 ${backgroundColor}`}>
+      <Header title={title} onBackClick={onBackClick ?? router.back} />
+
+      <ScrollView
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+        bounces={false}
+        overScrollMode="never"
+      >
+        {children}
+      </ScrollView>
+
+      <View className="px-4 pb-[22px] mb-safe">{footer}</View>
+    </SafeAreaView>
+  );
+}
