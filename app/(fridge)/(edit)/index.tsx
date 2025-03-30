4287834a10c012bc036467ce49015db78fabbab7
Jsueeee <suyeon.aos@gmail.com>
2025-03-30 17:01:57 +0900

    feat: 재료 수정 화면에 ScreenLayout 적용 및 CTA 클릭 핸들러 추가

--- app/(fridge)/(edit)/index.tsx
+++ app/(fridge)/(edit)/index.tsx
@@ -1,6 +1,7 @@
 import { useFridgeDetailQuery } from "@/app/hooks/queries/useFridgeDetailQuery";
 import { CTAButton } from "@/components/CTAButton";
 import { Header } from "@/components/Header";
+import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import { FoodDataManager } from "@/constants/IngredientManager";
 import i18n from "@/lib/i18n";
 import { router, useLocalSearchParams } from "expo-router";
@@ -17,35 +18,35 @@ export default function IngredientEditScreen() {
 
   const Icon = FoodDataManager.getImageSource(ingredient?.ingredientIconId);
 
+  const onCTAClick = () => {
+    console.log("CTA clicked");
+  };
+
   if (!ingredient) return null;
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <Header
-        title={ingredient.ingredientName}
-        onBackClick={() => {
-          router.back();
-        }}
-      />
+    <ScreenLayout
+      title={ingredient.ingredientName}
+      footer={
+        <View className="fixed bottom-0 left-0 right-0 px-4 pb-safe">
+          <Pressable className="items-center py-[14px]">
+            <Text className="text-title5 text-strong-destructive">
+              {i18n.t("edit_food.remove")}
+            </Text>
+          </Pressable>
 
+          <CTAButton
+            buttonLabel={i18n.t("edit_food.cta")}
+            onClick={onCTAClick}
+            disabled={ingredient.quantity <= 0}
+            className="mt-2"
+          />
+        </View>
+      }
+    >
       <View className="flex-1 px-4 pt-3 items-center">
         {Icon && <Icon width={100} height={100} className="rounded-full" />}
       </View>
-
-      <View className="fixed bottom-0 left-0 right-0 px-4 pb-safe">
-        <Pressable className="items-center py-[14px]">
-          <Text className="text-title5 text-strong-destructive">
-            {i18n.t("edit_food.remove")}
-          </Text>
-        </Pressable>
-
-        <CTAButton
-          buttonLabel={i18n.t("edit_food.cta")}
-          onClick={() => {}}
-          disabled={ingredient.quantity <= 0}
-          className="mb-[22px] mt-2"
-        />
-      </View>
-    </SafeAreaView>
+    </ScreenLayout>
   );
 }
