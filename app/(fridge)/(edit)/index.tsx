7612295f8874d95098700a0c40c5b683b60a3284
Jsueeee <suyeon.aos@gmail.com>
2025-04-04 22:13:25 +0900

    feat: 재료 수정 화면 데이터 관리 개선

--- app/(fridge)/(edit)/index.tsx
+++ app/(fridge)/(edit)/index.tsx
@@ -1,17 +1,16 @@
+import { useDeleteFridgeMutation } from "@/app/hooks/mutations/useDeleteFridgeMutation";
 import { useFridgeDetailQuery } from "@/app/hooks/queries/useFridgeDetailQuery";
 import { ChoiceDialog } from "@/components/ChoiceDialog";
 import { CTAButton } from "@/components/CTAButton";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import { FoodDataManager } from "@/constants/IngredientManager";
 import i18n from "@/lib/i18n";
-import { useLocalSearchParams } from "expo-router";
-import { useEffect, useRef, useState } from "react";
-import { Animated, Pressable, Text, View } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { Pressable, Text, View } from "react-native";
 import { EditExpiredAtMenu } from "./components/EditExpiredAtMenu";
 import { EditQuantityMenu } from "./components/EditQuantityMenu";
 import { EditUnitMenu } from "./components/EditUnitMenu";
-import { useDeleteFridgeMutation } from "@/app/hooks/mutations/useDeleteFridgeMutation";
-import { useRouter } from "expo-router";
 
 export default function IngredientEditScreen() {
   const { id } = useLocalSearchParams();
@@ -25,14 +24,15 @@ export default function IngredientEditScreen() {
 
   const Icon = FoodDataManager.getImageSource(ingredient?.ingredientIconId);
 
-  const [localQuantity, setLocalQuantity] = useState(ingredient?.quantity);
-  const [localUnit, setLocalUnit] = useState(ingredient?.unit);
-  const [localExpiredAt, setLocalExpiredAt] = useState(
-    ingredient?.expiredAt ? new Date(ingredient.expiredAt) : null
-  );
+  const [localData, setLocalData] = useState(ingredient);
+
   const [removeDialogVisible, setRemoveDialogVisible] = useState(false);
 
   const deleteFridgeMutation = useDeleteFridgeMutation();
+
+  useEffect(() => {
+    setLocalData(ingredient);
+  }, [ingredient]);
 
   const onCTAClick = () => {
     console.log("CTA clicked");
@@ -56,6 +56,12 @@ export default function IngredientEditScreen() {
     setRemoveDialogVisible(false);
   };
 
+  const updateLocalData = (update: Partial<typeof localData>) => {
+    if (localData) {
+      setLocalData({ ...localData, ...update });
+    }
+  };
+
   if (!ingredient) return null;
 
   return (
@@ -72,7 +78,7 @@ export default function IngredientEditScreen() {
           <CTAButton
             buttonLabel={i18n.t("edit_food.cta")}
             onClick={onCTAClick}
-            disabled={!localQuantity || localQuantity <= 0}
+            disabled={!localData?.quantity || localData.quantity <= 0}
             className="mt-2"
           />
         </View>
@@ -84,16 +90,23 @@ export default function IngredientEditScreen() {
         <View className="h-3" />
 
         <EditQuantityMenu
-          quantity={localQuantity ?? 0}
-          onQuantityChanged={setLocalQuantity}
+          quantity={localData?.quantity ?? 0}
+          onQuantityChanged={(quantity) => updateLocalData({ quantity })}
         />
 
         <EditExpiredAtMenu
-          expiredAt={localExpiredAt}
-          onExpiredAtChanged={setLocalExpiredAt}
+          expiredAt={
+            localData?.expiredAt ? new Date(localData.expiredAt) : null
+          }
+          onExpiredAtChanged={(expiredAt) => {
+            updateLocalData({ expiredAt: expiredAt.toISOString() });
+          }}
         />
 
-        <EditUnitMenu unit={localUnit} onUnitChanged={setLocalUnit} />
+        <EditUnitMenu
+          unit={localData?.unit}
+          onUnitChanged={(unit) => updateLocalData({ unit })}
+        />
       </View>
 
       <ChoiceDialog
