87c17584669bffdd317c685c5d145e6dc3d40414
Jsueeee <suyeon.aos@gmail.com>
2025-04-06 02:21:25 +0900

    feat: 뷰 및 좋아요 컴포넌트 추가

---
+++ app/(tabs)/(recipe)/components/RecipeViewScrapCount.tsx
@@ -0,0 +1,37 @@
+import HeartFillIcon from "@/assets/images/ic_heart_fill.svg";
+import HeartStrokeIcon from "@/assets/images/ic_heart_stroke.svg";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+interface Props {
+  viewCount?: number;
+  scrapCount: number;
+  isScrapped: boolean;
+  onScrapClick?: () => void;
+}
+
+const RecipeViewScrapCount: React.FC<Props> = ({
+  viewCount,
+  scrapCount,
+  isScrapped,
+  onScrapClick = () => {},
+}) => {
+  // TODO : 조회수 추후 추가 예정
+
+  return (
+    <TouchableOpacity
+      onPress={onScrapClick}
+      className="flex-row items-center gap-[2px]"
+    >
+      {isScrapped ? (
+        <HeartFillIcon color="#F3734F" width={16} height={16} />
+      ) : (
+        <HeartStrokeIcon color="#3F4542" width={16} height={16} />
+      )}
+
+      <Text className="text-body4 text-text-normal">{scrapCount}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default RecipeViewScrapCount;
