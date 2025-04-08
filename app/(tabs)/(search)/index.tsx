ff0a63af01ad9d42029fc542d097c2b19d7b1686
Jsueeee <suyeon.aos@gmail.com>
2025-04-09 01:53:09 +0900

    feat(search): 검색 취소 버튼 추가

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -1,7 +1,9 @@
+import { PressableScale } from "@/app/components/PressableScale";
 import { MainTabHeader } from "@/components/MainTabHeader";
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import { SearchBar } from "./components/SearchBar";
+import i18n from "@/lib/i18n";
 
 export default function SearchScreen() {
   const [keyword, setKeyword] = useState("");
@@ -10,11 +12,21 @@ export default function SearchScreen() {
     <SafeAreaView className="flex-1 bg-white">
       <MainTabHeader tab="search" />
 
-      <SearchBar
-        keyword={keyword}
-        onValueChange={setKeyword}
-        onSearch={() => {}}
-      />
+      <View className="flex-1 flex-row gap-[3px] px-4">
+        <SearchBar
+          keyword={keyword}
+          onValueChange={setKeyword}
+          onSearch={() => {}}
+          className="flex-1"
+        />
+
+        {/* 취소 버튼 */}
+        <PressableScale onPress={() => setKeyword("")} className="p-2.5">
+          <Text className="text-body2 text-text-strong">
+            {i18n.t("search.search_cancel")}
+          </Text>
+        </PressableScale>
+      </View>
     </SafeAreaView>
   );
 }
