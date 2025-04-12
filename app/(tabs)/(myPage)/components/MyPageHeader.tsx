13c76a006bc2f03d0e1b968fb907277fdadd8ac5
Jsueeee <suyeon.aos@gmail.com>
2025-04-13 02:06:52 +0900

    chore: 마이페이지 헤더 설정 버튼 PressableScale로 감싸기

--- app/(tabs)/(myPage)/components/MyPageHeader.tsx
+++ app/(tabs)/(myPage)/components/MyPageHeader.tsx
@@ -1,19 +1,24 @@
+import { PressableScale } from "@/app/components/PressableScale";
 import SettingIcon from "@/assets/images/ic_setting.svg";
 import { MainTabHeader } from "@/components/MainTabHeader";
 import React from "react";
 import { View } from "react-native";
 
 export function MyPageHeader() {
+  const onSettingButtonPress = () => {
+    console.log("setting button press");
+  };
+
   return (
     <View className="flex-1 flex-row justify-between">
       <MainTabHeader tab="myPage" />
 
-      <SettingIcon
-        width={24}
-        height={24}
-        hitSlop={8}
-        style={{ marginRight: 16, alignSelf: "flex-end", marginBottom: 16 }}
-      />
+      <PressableScale
+        onPress={onSettingButtonPress}
+        className="self-end mr-4 mb-4"
+      >
+        <SettingIcon width={24} height={24} hitSlop={8} />
+      </PressableScale>
     </View>
   );
 }
