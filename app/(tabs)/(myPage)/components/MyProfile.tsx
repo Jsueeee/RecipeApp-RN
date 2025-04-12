938d2bced7b492cd4ad493d7c557faa0c23c3662
Jsueeee <suyeon.aos@gmail.com>
2025-04-13 01:36:25 +0900

    feat: 마이페이지 프로필 컴포넌트 추가 및 사용자 정보 표시 기능 구현

---
+++ app/(tabs)/(myPage)/components/MyProfile.tsx
@@ -0,0 +1,34 @@
+import { PressableScale } from "@/app/components/PressableScale";
+import RightArrowIcon from "@/assets/images/ic_arrow_right.svg";
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+interface Props {
+  profileImage?: string | undefined | null;
+  nickname: string | undefined;
+  onPress?: () => void;
+}
+
+export function MyProfile({
+  profileImage,
+  nickname,
+  onPress = () => {},
+}: Props) {
+  return (
+    <View className="flex-row items-center mx-4 mt-3">
+      <Image
+        source={{ uri: profileImage ?? "" }}
+        className="w-12 h-12 rounded-[18px] bg-gray-50"
+        resizeMode="cover"
+      />
+
+      <Text className="flex-1 ml-3 text-title4 text-text-strong">
+        {nickname}
+      </Text>
+
+      <PressableScale onPress={onPress} className="w-6 h-6 rounded-lg">
+        <RightArrowIcon width={24} height={24} />
+      </PressableScale>
+    </View>
+  );
+}
--- app/(tabs)/(myPage)/index.tsx
+++ app/(tabs)/(myPage)/index.tsx
@@ -1,14 +1,26 @@
+import { useUserInfoQuery } from "@/app/hooks/queries/useUserInfoQuery";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import React from "react";
 import { MyPageHeader } from "./components/MyPageHeader";
-import { useUserInfoQuery } from "@/app/hooks/queries/useUserInfoQuery";
+import { MyProfile } from "./components/MyProfile";
 
 export default function MyPageScreen() {
   const { data: userInfo } = useUserInfoQuery();
 
+  const onProfilePress = () => {
+    console.log("profile press");
+    // TODO : 프로필 설정 페이지로 이동
+  };
+
   return (
     <ScreenLayout isShowHeader={false} backgroundColor="background-alternative">
       <MyPageHeader />
+
+      <MyProfile
+        profileImage={userInfo?.profileImageUrl}
+        nickname={userInfo?.nickname}
+        onPress={onProfilePress}
+      />
     </ScreenLayout>
   );
 }
