93b491efc8528f4d5aa47ea0b2d61588d264bcb1
Jsueeee <suyeon.aos@gmail.com>
2025-04-13 02:43:04 +0900

    feat: 프로필 이미지와 닉네임 컴포넌트 추가

--- app/(myPage)/(profile)/index.tsx
+++ app/(myPage)/(profile)/index.tsx
@@ -1,16 +1,47 @@
+import { PressableScale } from "@/app/components/PressableScale";
+import { useUserInfoQuery } from "@/app/hooks/queries/useUserInfoQuery";
+import ProfileImageEditIcon from "@/assets/images/ic_profile_image_edit.svg";
+import NicknameEditIcon from "@/assets/images/ic_profile_nickname_edit.svg";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function MyProfileScreen() {
+  const { data: userInfo } = useUserInfoQuery();
+
+  const onProfileImageEditButtonPress = () => {
+    console.log("profile image edit button pressed");
+  };
+
+  const onNicknameEditButtonPress = () => {
+    console.log("nickname edit button pressed");
+  };
+
   return (
     <ScreenLayout
       title={i18n.t("profile.title")}
       backgroundColor="background-alternative"
     >
-      <View className="flex-1 items-center justify-center">
-        <Text>My Profile</Text>
+      <View className="items-center justify-center gap-4 mt-3">
+        <PressableScale onPress={onProfileImageEditButtonPress}>
+          <Image
+            source={{ uri: userInfo?.profileImageUrl ?? "" }}
+            className="w-[100px] h-[100px] rounded-[36px] bg-gray-50"
+          />
+          <ProfileImageEditIcon
+            width={32}
+            height={32}
+            style={{ position: "absolute", bottom: 0, right: -8 }}
+          />
+        </PressableScale>
+
+        <PressableScale onPress={onNicknameEditButtonPress}>
+          <View className="flex-row items-center justify-center gap-1 mx-4">
+            <Text>{userInfo?.nickname}</Text>
+            <NicknameEditIcon width={20} height={20} />
+          </View>
+        </PressableScale>
       </View>
     </ScreenLayout>
   );
