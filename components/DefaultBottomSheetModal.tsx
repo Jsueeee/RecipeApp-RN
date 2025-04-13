9a948e7dfbafa640b724855a590da01a2f4a085f
Jsueeee <suyeon.aos@gmail.com>
2025-04-13 16:57:42 +0900

    feat: 기본 바텀시트 모달 컴포넌트 추가

---
+++ components/DefaultBottomSheetModal.tsx
@@ -0,0 +1,63 @@
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { useCallback } from "react";
+import { Animated, Pressable, StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface Props {
+  bottomSheetModalRef: React.RefObject<BottomSheetModal>;
+  children: React.ReactNode;
+}
+
+/**
+ * 앱 내에서 기본으로 사용할 바텀시트
+ */
+export default function DefaultBottomSheetModal({
+  bottomSheetModalRef,
+  children,
+}: Props) {
+  const insets = useSafeAreaInsets();
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+
+    if (index === -1) {
+      bottomSheetModalRef.current?.dismiss();
+    }
+  }, []);
+
+  const onBackDropPress = () => {
+    bottomSheetModalRef.current?.dismiss();
+  };
+
+  return (
+    <BottomSheetModal
+      ref={bottomSheetModalRef}
+      onChange={handleSheetChanges}
+      backgroundStyle={{ backgroundColor: "white" }}
+      handleComponent={null}
+      enableDismissOnClose={true}
+      enablePanDownToClose={true}
+      backdropComponent={({ animatedIndex, style }) => (
+        <Pressable onPress={onBackDropPress} style={style}>
+          <Animated.View style={styles.backdrop} />
+        </Pressable>
+      )}
+    >
+      <BottomSheetView
+        style={[styles.contentContainer, { paddingBottom: insets.bottom }]}
+      >
+        {children}
+      </BottomSheetView>
+    </BottomSheetModal>
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  backdrop: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+});
