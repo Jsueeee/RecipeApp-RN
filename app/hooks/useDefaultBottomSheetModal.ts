e628476cceed2bcfddbb9af190806894a4878754
Jsueeee <suyeon.aos@gmail.com>
2025-04-13 16:57:50 +0900

    feat: 기본 바텀시트 모달 훅 추가

---
+++ app/hooks/useDefaultBottomSheetModal.ts
@@ -0,0 +1,20 @@
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useCallback, useRef } from "react";
+
+export const useDefaultBottomSheetModal = () => {
+  const ref = useRef<BottomSheetModal>(null);
+
+  const open = useCallback(() => {
+    ref.current?.present();
+  }, []);
+
+  const dismiss = useCallback(() => {
+    ref.current?.dismiss();
+  }, []);
+
+  return {
+    ref,
+    open,
+    dismiss,
+  };
+};
