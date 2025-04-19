bd10ab0ecee5c4c858352e94eeb8403d6b2a1e95
Jsueeee <suyeon.aos@gmail.com>
2025-04-19 22:34:11 +0900

    feat: 스크랩 레시피 리스트 화면 추가

---
+++ app/(myPage)/(scrap)/index.tsx
@@ -0,0 +1,138 @@
+import SmallRecipeListItem from "@/app/(recipe)/components/SmallRecipeListItem";
+import { useScrapRecipesQuery } from "@/app/hooks/queries/useScrapRecipesQuery";
+import { RecipeSummary } from "@/app/types/domain/recipe";
+import { DotLoading } from "@/components/DotLoading";
+import { DotLoadingScreen } from "@/components/DotLoadingScreen";
+import { EmptyPlaceholder } from "@/components/EmptyPlaceholder";
+import { ScreenLayout } from "@/components/layout/ScreenLayout";
+import { RecipeSourceTypeTabRow } from "@/components/RecipeSourceTypeTabRow";
+import {
+  RECIPE_SOURCE_TYPE,
+  RecipeSourceType,
+} from "@/constants/RecipeSourceType";
+import i18n from "@/lib/i18n";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { FlatList, Linking, View } from "react-native";
+
+export default function MyScrapScreen() {
+  const [selectedTab, setSelectedTab] = useState<RecipeSourceType>(
+    RECIPE_SOURCE_TYPE.BLOG
+  );
+
+  const {
+    data: recipeList,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+  } = useScrapRecipesQuery(selectedTab);
+
+  /**
+   * 블로그, 유튜브 레시피 클릭 시 링크 이동
+   * 추천 레시피 클릭 시 상세 페이지로 이동
+   */
+  const onRecipeItemPress = (recipe: RecipeSummary) => {
+    switch (selectedTab) {
+      case RECIPE_SOURCE_TYPE.BLOG:
+      case RECIPE_SOURCE_TYPE.YOUTUBE:
+        if (recipe.linkUrl) {
+          Linking.openURL(recipe.linkUrl);
+        }
+        break;
+      default:
+        router.push(`/(recipe)/(detail)?id=${recipe.id}`);
+        break;
+    }
+  };
+
+  const ListFooterComponent = () =>
+    hasNextPage ? <DotLoading className="mb-20" /> : null;
+
+  const ItemSeparator = () => (
+    <View className="w-full mx-4 h-[1px] bg-line-alternative" />
+  );
+
+  const keyExtractor = (item: RecipeSummary) => item.id.toString();
+
+  const onEndReached = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const getItemLayout = (
+    data: ArrayLike<RecipeSummary> | null | undefined,
+    index: number
+  ) => ({
+    length: 164,
+    offset: 164 * index,
+    index,
+  });
+
+  const renderItem = ({
+    item,
+    index,
+  }: {
+    item: RecipeSummary;
+    index: number;
+  }) => (
+    <View className="bg-white">
+      <SmallRecipeListItem
+        recipeId={item.id}
+        title={item.title}
+        thumbnail={item.thumbnail}
+        postUserName={item.postUserName ?? null}
+        postDate={item.postDate}
+        viewCount={item.viewCount}
+        scrapCount={item.scrapCount}
+        isScrapped={item.isScrapped}
+        onPress={() => onRecipeItemPress(item)}
+      />
+    </View>
+  );
+
+  const renderContent = () => {
+    if (isLoading) return <DotLoadingScreen />;
+
+    const recipes = recipeList?.recipes;
+
+    if (!recipes?.length) {
+      return (
+        <EmptyPlaceholder
+          title={i18n.t("my_page.scrap_list_empty_placeholder_title")}
+          description={i18n.t("my_page.scrap_list_empty_placeholder_desc")}
+        />
+      );
+    }
+
+    return (
+      <FlatList
+        data={recipes}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        ListFooterComponent={ListFooterComponent}
+        onEndReached={onEndReached}
+        ItemSeparatorComponent={ItemSeparator}
+        onEndReachedThreshold={0.5}
+        className="bg-white"
+        contentContainerStyle={{ paddingBottom: 24 }}
+        getItemLayout={getItemLayout}
+        bounces={false}
+        alwaysBounceVertical={false}
+        showsVerticalScrollIndicator={false}
+      />
+    );
+  };
+
+  return (
+    <ScreenLayout title={i18n.t("my_page.my_recipe")} isScrollEnabled={false}>
+      <RecipeSourceTypeTabRow
+        tabs={Object.values(RECIPE_SOURCE_TYPE)}
+        selectedTab={selectedTab}
+        onTabSelected={setSelectedTab}
+      />
+
+      {renderContent()}
+    </ScreenLayout>
+  );
+}
