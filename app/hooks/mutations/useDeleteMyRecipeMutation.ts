4494d5bd840321c9642e4c017cadffe86f018ec4
Jsueeee <suyeon.aos@gmail.com>
2025-04-20 00:35:42 +0900

    feat: 나만의 레시피 삭제 뮤테이션 훅에 성공 및 오류 콜백 추가

--- app/hooks/mutations/useDeleteMyRecipeMutation.ts
+++ app/hooks/mutations/useDeleteMyRecipeMutation.ts
@@ -1,9 +1,16 @@
 import { apiClient } from "@/app/lib/api/client";
+import { MutationCallbacks } from "@/app/types/common/mutation";
 import { useMutation } from "@tanstack/react-query";
 
-export const useDeleteMyRecipeMutation = () => {
+export const useDeleteMyRecipeMutation = (callbacks?: MutationCallbacks) => {
   const deleteMutation = useMutation({
     mutationFn: (recipeId: number) => apiClient.delete(`/recipes/${recipeId}`),
+    onSuccess: () => {
+      callbacks?.onSuccess?.();
+    },
+    onError: (error) => {
+      callbacks?.onError?.(error);
+    },
   });
 
   return {
