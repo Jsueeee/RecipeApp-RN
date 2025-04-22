fd7d01afcfa4af46d2f14a180240c6daeb99246e
Jsueeee <suyeon.aos@gmail.com>
2025-04-23 00:48:15 +0900

    feat: 재료 선택 화면에 선택된 재료 관리 기능 및 리스트 렌더링 추가

--- app/(ingredient)/(pick)/index.tsx
+++ app/(ingredient)/(pick)/index.tsx
@@ -1,25 +1,46 @@
 import { CategoryTabs } from "@/app/(tabs)/(fridge)/components/CategoryTabs";
 import { FridgeTabs } from "@/app/(tabs)/(fridge)/constants/fridgeTabs";
 import { useIngredientsQuery } from "@/app/hooks/queries/useMyIngredientsQuery";
+import { CategorizedPickIngredients } from "@/app/types/domain/ingredient";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
 import React, { useRef, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { FlatList, ScrollView, View } from "react-native";
+import { CategorizedPickIngredientsWithIconGroup } from "./components/CategorizedPickIngredientsWithIconGroup";
 
 const TABS = Object.values(FridgeTabs);
 
 export default function IngredientPickScreen() {
   const scrollViewRef = useRef<ScrollView>(null);
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-
-  const { data: ingredients } = useIngredientsQuery({
-    keyword: TABS[selectedTabIndex],
-  });
+  const [selectedIngredients, setSelectedIngredients] = useState<number[]>([]);
+  const { data: ingredients } = useIngredientsQuery();
 
   const handleTabSelect = (index: number) => {
     setSelectedTabIndex(index);
     scrollViewRef.current?.scrollTo({ y: 0, animated: true });
   };
+
+  const handleIngredientSelect = (ingredientId: number) => {
+    setSelectedIngredients([...selectedIngredients, ingredientId]);
+  };
+
+  const handleIngredientUnselect = (ingredientId: number) => {
+    setSelectedIngredients(
+      selectedIngredients.filter((id) => id !== ingredientId)
+    );
+  };
+
+  const renderItem = ({ item }: { item: CategorizedPickIngredients }) => (
+    <CategorizedPickIngredientsWithIconGroup
+      key={item.ingredientCategoryId}
+      categoryName={item.ingredientCategoryName}
+      ingredients={item.ingredients}
+      selectedIngredients={selectedIngredients}
+      onSelect={handleIngredientSelect}
+      onUnselect={handleIngredientUnselect}
+    />
+  );
 
   return (
     <ScreenLayout title={i18n.t("ingredient_pick.title")}>
@@ -31,6 +52,14 @@ export default function IngredientPickScreen() {
           className="bg-white w-full"
         />
       </View>
+
+      <FlatList
+        data={ingredients}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={{ gap: 20 }}
+      />
     </ScreenLayout>
   );
 }
