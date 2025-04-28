1c66d6199a0911af990035007de761198a5b5af4
Jsueeee <suyeon.aos@gmail.com>
2025-04-28 22:22:35 +0900

    style: DotLoadingScreen 컴포넌트의 스타일을 수정하여 전체 화면을 차지하도록 변경

--- components/DotLoadingScreen.tsx
+++ components/DotLoadingScreen.tsx
@@ -9,9 +9,9 @@ interface Props {
 export const DotLoadingScreen = ({ className }: Props) => {
   return (
     <View
-      className={`absolute w-full h-full items-center justify-center ${className}`}
+      className={`absolute left-0 right-0 top-0 bottom-0 items-center justify-center ${className} z-10`}
     >
-      <TealDotLoading className="absolute" />
+      <TealDotLoading />
     </View>
   );
 };
