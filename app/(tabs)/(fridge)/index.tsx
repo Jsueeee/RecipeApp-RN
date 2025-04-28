2f5724d7028812d8258de649fa376cd598377d49
suyeon <66951780+Jsueeee@users.noreply.github.com>
2025-04-28 23:05:05 +0900

    feat(fridge): 냉장고 화면 EmptyPlaceholder 및 Loading 처리 (#23)
    * feat: 냉장고 화면에서 로딩 및 빈 상태 처리 추가
    
    * fix: IngredientItem 컴포넌트에서 텍스트 클래스 이름 수정

--- app/(tabs)/(fridge)/index.tsx
+++ app/(tabs)/(fridge)/index.tsx
@@ -1,17 +1,18 @@
 import { CategoryTabs } from "@/app/(tabs)/(fridge)/components/CategoryTabs";
 import { useFridgesQuery } from "@/app/hooks/queries/useFridgeQuery";
 import { Ingredient } from "@/app/types/domain/fridge";
+import { DotLoadingScreen } from "@/components/DotLoadingScreen";
 import { EmptyPlaceholder } from "@/components/EmptyPlaceholder";
 import { MainTabHeader } from "@/components/MainTabHeader";
+import i18n from "@/lib/i18n";
 import { router, Stack } from "expo-router";
 import React, { useMemo, useRef, useState } from "react";
 import { ScrollView, View } from "react-native";
+import { SystemBars } from "react-native-edge-to-edge";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AddIngredientButton } from "./components/AddIngredientButton";
 import { CategorizedIngredientsGroup } from "./components/CategorizedIngredientsGroup";
 import { FridgeTabs } from "./constants/fridgeTabs";
-import i18n from "@/lib/i18n";
-import { SystemBars } from "react-native-edge-to-edge";
 
 const TABS = Object.values(FridgeTabs);
 
@@ -19,7 +20,7 @@ export default function FridgeScreen() {
   const scrollViewRef = useRef<ScrollView>(null);
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
-  const { data: fridges } = useFridgesQuery();
+  const { fridges, isLoading } = useFridgesQuery();
 
   const onIngredientItemClick = (ingredient: Ingredient) => {
     router.push({
@@ -39,15 +40,45 @@ export default function FridgeScreen() {
 
   const filteredCategories = useMemo(
     () =>
-      fridges?.categories
-        .filter(
+      fridges
+        ?.filter(
           (category) =>
             selectedTabIndex === 0 ||
             category.categoryName === TABS[selectedTabIndex]
         )
         .filter((category) => category.ingredients.length > 0),
-    [fridges?.categories, selectedTabIndex]
+    [fridges, selectedTabIndex]
   );
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <DotLoadingScreen />;
+    }
+
+    if (filteredCategories?.length === 0) {
+      return (
+        <View className="flex-1 justify-center items-center">
+          <EmptyPlaceholder
+            title={i18n.t("home.fridge_is_empty")}
+            description={i18n.t("home.fridge_is_empty_sub")}
+          />
+        </View>
+      );
+    }
+
+    return (
+      <View className="flex-1 pt-2">
+        {filteredCategories?.map((category) => (
+          <CategorizedIngredientsGroup
+            key={category.categoryName}
+            categoryName={category.categoryName}
+            ingredients={category.ingredients}
+            onIngredientItemClick={onIngredientItemClick}
+          />
+        ))}
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-background-alternative">
@@ -72,27 +103,7 @@ export default function FridgeScreen() {
             className="bg-gray-100"
           />
 
-          <View className="px-4 flex-1">
-            {!filteredCategories || filteredCategories.length === 0 ? (
-              <View className="flex-1 justify-center items-center">
-                <EmptyPlaceholder
-                  title={i18n.t("home.fridge_is_empty")}
-                  description={i18n.t("home.fridge_is_empty_sub")}
-                />
-              </View>
-            ) : (
-              <View className="flex-1 pt-2">
-                {filteredCategories.map((category) => (
-                  <CategorizedIngredientsGroup
-                    key={category.categoryName}
-                    categoryName={category.categoryName}
-                    ingredients={category.ingredients}
-                    onIngredientItemClick={onIngredientItemClick}
-                  />
-                ))}
-              </View>
-            )}
-          </View>
+          <View className="px-4 flex-1">{renderContent()}</View>
         </ScrollView>
 
         <AddIngredientButton onPress={handleAddPress} />
