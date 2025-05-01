dce07706dbd02991b9cf57ff6d8d182222ae0006
Jsueeee <suyeon.aos@gmail.com>
2025-05-01 22:33:25 +0900

    feat: 냉장고 바구니 화면에 CTA 버튼 및 그라데이션 배경 추가

--- app/(fridge)/(basket)/index.tsx
+++ app/(fridge)/(basket)/index.tsx
@@ -2,10 +2,13 @@ import { CategorizedIngredientsGroup } from "@/app/(tabs)/(fridge)/components/Ca
 import { useFridgeBasketQuery } from "@/app/hooks/queries/useFridgeBasketQuery";
 import { CategorizedFridgeBasket, Ingredient } from "@/app/types/domain/fridge";
 import { mapFridgeBasketIngredient } from "@/app/types/mappers/fridge";
+import { CTAButton } from "@/components/CTAButton";
 import { DotLoadingScreen } from "@/components/DotLoadingScreen";
 import { EmptyPlaceholder } from "@/components/EmptyPlaceholder";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
+import { LinearGradient } from "expo-linear-gradient";
+
 import { FlatList, View } from "react-native";
 
 export default function IngredientBasketScreen() {
@@ -68,6 +71,22 @@ export default function IngredientBasketScreen() {
       backgroundColor="bg-alternative"
     >
       {renderContent()}
+
+      <View className="absolute bottom-0 left-0 right-0">
+        <LinearGradient
+          colors={["rgba(255,255,255,0)", "rgba(255,255,255,1)"]}
+          style={{ height: 40 }}
+        />
+
+        <View className="bg-white px-4 pb-[22px]">
+          <CTAButton
+            buttonLabel={i18n.t("fridge_basket.cta")}
+            onPress={() => {
+              console.log("cta");
+            }}
+          />
+        </View>
+      </View>
     </ScreenLayout>
   );
 }
