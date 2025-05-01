d61e12808b5bb4ee4950b939b48aa5aca178fb81
Jsueeee <suyeon.aos@gmail.com>
2025-05-01 22:41:07 +0900

    feat: 냉장고에 아이템 추가를 위한 뮤테이션 훅 추가

---
+++ app/hooks/mutations/usePostFridgeMutation.ts
@@ -0,0 +1,35 @@
+import { apiClient } from "@/app/lib/api/client";
+import { queryClient } from "@/app/lib/query/client";
+import { QUERY_KEYS } from "@/app/lib/query/keys";
+import { MutationCallbacks } from "@/app/types/common/mutation";
+import { useMutation } from "@tanstack/react-query";
+
+export const usePostFridgeMutation = (callbacks?: MutationCallbacks) => {
+  const postFridgeMutation = useMutation({
+    mutationKey: QUERY_KEYS.FRIDGE.FRIDGES,
+    mutationFn: () => apiClient.post("/fridges"),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.FRIDGE.FRIDGES,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.FRIDGE.BASKET,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.RECIPE.RECOMMENDED_LIST,
+      });
+
+      callbacks?.onSuccess?.();
+    },
+    onError: (error) => {
+      callbacks?.onError?.(error);
+    },
+  });
+
+  return {
+    postFridge: postFridgeMutation.mutateAsync,
+    isPending: postFridgeMutation.isPending,
+  };
+};
