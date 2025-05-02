6fe413565270f84c66c47784468ad8446f8ef61d
Jsueeee <suyeon.aos@gmail.com>
2025-05-03 03:09:35 +0900

    feat: 액세스 토큰, 리프레시 토큰 및 사용자 ID 삭제하는 clear() 추가

--- app/lib/storage/auth.ts
+++ app/lib/storage/auth.ts
@@ -29,4 +29,9 @@ export const authStorage = {
     await SecureStore.deleteItemAsync(AUTH_KEYS.REFRESH_TOKEN);
     await SecureStore.deleteItemAsync(AUTH_KEYS.USER_ID);
   },
+  clear: async () => {
+    await SecureStore.deleteItemAsync(AUTH_KEYS.ACCESS_TOKEN);
+    await SecureStore.deleteItemAsync(AUTH_KEYS.REFRESH_TOKEN);
+    await SecureStore.deleteItemAsync(AUTH_KEYS.USER_ID);
+  },
 };
