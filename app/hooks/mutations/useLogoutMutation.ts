46d234f355dc950da0eec9f4a47f15ab662752d8
Jsueeee <suyeon.aos@gmail.com>
2025-05-03 03:09:56 +0900

    feat: 카카오 로그아웃 뮤테이션 훅 추가 및 성공 시 라우터 처리

---
+++ app/hooks/mutations/useLogoutMutation.ts
@@ -0,0 +1,31 @@
+import { QUERY_KEYS } from "@/app/lib/query/keys";
+import { authStorage } from "@/app/lib/storage/auth";
+import { MutationCallbacks } from "@/app/types/common/mutation";
+import { logout as logoutKakao } from "@react-native-kakao/user";
+import { useMutation } from "@tanstack/react-query";
+import { router } from "expo-router";
+
+export const useLogoutMutation = (callbacks?: MutationCallbacks) => {
+  const kakaoLogoutMutation = useMutation({
+    mutationKey: QUERY_KEYS.AUTH.KAKAO,
+    mutationFn: async () => {
+      try {
+        await logoutKakao();
+      } catch (error) {
+        console.warn("Logout API failed", error);
+      }
+
+      await authStorage.clear();
+    },
+    onSuccess: () => {
+      router.dismissAll();
+      router.replace("/(auth)");
+    },
+  });
+
+  return {
+    kakaoLogout: kakaoLogoutMutation.mutateAsync,
+    isLoading: kakaoLogoutMutation.isPending,
+    error: kakaoLogoutMutation.error,
+  };
+};
