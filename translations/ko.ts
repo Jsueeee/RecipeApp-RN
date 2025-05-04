672a80c9f0d738def46741117e9a8c5282e10f7b
Jsueeee <suyeon.aos@gmail.com>
2025-05-05 00:53:42 +0900

    feat: 나만의 재료 추가 버튼 및 치즈 이미지 컴포넌트 추가

--- translations/ko.ts
+++ translations/ko.ts
@@ -371,6 +371,7 @@ export default {
       "{{ingredientName}}를 나만의 재료에서 삭제 하시겠어요?\n삭제한 재료는 복구되지 않습니다.",
     delete_dialog_cancel: "닫기",
     delete_dialog_confirm: "삭제하기",
+    create_custom_ingredient_button: "나만의 재료 추가하기",
   },
   fridge_basket: {
     header: "냉장고 바구니",
