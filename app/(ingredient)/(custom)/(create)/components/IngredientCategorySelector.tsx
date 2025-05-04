0cdda73fd1371224043e4f9b04b7eedffec22864
Jsueeee <suyeon.aos@gmail.com>
2025-05-05 03:28:49 +0900

    feat: IngredientCategorySelector에서 selectedCategoryId 타입을 number | null로 변경

--- app/(ingredient)/(custom)/(create)/components/IngredientCategorySelector.tsx
+++ app/(ingredient)/(custom)/(create)/components/IngredientCategorySelector.tsx
@@ -7,7 +7,7 @@ import i18n from "@/lib/i18n";
 import { Text, View } from "react-native";
 
 interface Props {
-  selectedCategoryId: number;
+  selectedCategoryId: number | null;
   onCategoryChanged: (value: number) => void;
 }
 
