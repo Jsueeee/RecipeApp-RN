b290ba3a52e9064015512e9dd7e227d798d65349
Jsueeee <suyeon.aos@gmail.com>
2025-05-05 00:12:32 +0900

    feat: 오른쪽 상단에 제거 버튼이 달린 재료 제거 아이템 컴포넌트 RemoveIngredientItem 추가

---
+++ components/RemoveIngredientItem.tsx
@@ -0,0 +1,63 @@
+import { PressableScale } from "@/app/components/PressableScale";
+import RemoveIcon from "@/assets/images/ic_selected_cancel.svg";
+import { FoodDataManager } from "@/constants/IngredientManager";
+import React, { useMemo } from "react";
+import { Text, View } from "react-native";
+
+interface Props {
+  ingredientId: number;
+  ingredientName: string;
+  ingredientIconId: number | null;
+  onRemovePress: () => void;
+}
+
+const iconCache = new Map<number, React.ComponentType<any>>();
+
+export const RemoveIngredientItem = React.memo(
+  function RemoveIngredientItem({
+    ingredientId,
+    ingredientName,
+    ingredientIconId,
+    onRemovePress,
+  }: Props) {
+    const Icon = useMemo(() => {
+      if (!ingredientIconId) return null;
+
+      if (iconCache.has(ingredientIconId)) {
+        return iconCache.get(ingredientIconId)!;
+      }
+
+      const icon = FoodDataManager.getImageSource(ingredientIconId);
+      if (icon) {
+        iconCache.set(ingredientIconId, icon);
+      }
+      return icon;
+    }, [ingredientIconId]);
+
+    return (
+      <View key={ingredientId} className="items-center w-[76px]">
+        <View className="relative w-[60px] h-[60px] justify-center items-center">
+          {Icon && <Icon width={60} height={60} />}
+
+          <PressableScale
+            onPress={onRemovePress}
+            className="absolute top-0 -right-3"
+            hitSlop={4}
+          >
+            <RemoveIcon width={32} height={32} />
+          </PressableScale>
+        </View>
+
+        <Text className="text-utility3 text-text-normal text-center">
+          {ingredientName}
+        </Text>
+      </View>
+    );
+  },
+  (prevProps, nextProps) => {
+    if (prevProps.ingredientId !== nextProps.ingredientId) return false;
+    if (prevProps.ingredientIconId !== nextProps.ingredientIconId) return false;
+    if (prevProps.ingredientName !== nextProps.ingredientName) return false;
+    return true;
+  }
+);
