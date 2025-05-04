50bedbf0a8d00b7fa279e3fe7aa96c8b2afe8387
Jsueeee <suyeon.aos@gmail.com>
2025-05-05 01:09:06 +0900

    feat: 커스텀 재료 만들기 화면에 재료 선택 버튼 추가

--- app/(ingredient)/(custom)/(create)/index.tsx
+++ app/(ingredient)/(custom)/(create)/index.tsx
@@ -1,13 +1,30 @@
+import { PressableScale } from "@/app/components/PressableScale";
+import SelectIngredientIconImage from "@/assets/images/img_select_ingredient_icon.svg";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
-import React from "react";
+import React, { useCallback } from "react";
 import { View } from "react-native";
 
 export default function CustomIngredientCreateScreen() {
+  const onSelectIconPress = useCallback(() => {
+    console.log("select icon");
+  }, []);
+
   return (
     <>
-      <ScreenLayout title={i18n.t("custom_ingredient.create_title")}>
-        <View></View>
+      <ScreenLayout
+        title={i18n.t("custom_ingredient.create_title")}
+        isScrollEnabled={true}
+      >
+        <View className="flex-1 px-4 py-3">
+          <PressableScale onPress={onSelectIconPress}>
+            <SelectIngredientIconImage
+              width={100}
+              height={100}
+              style={{ alignSelf: "center" }}
+            />
+          </PressableScale>
+        </View>
       </ScreenLayout>
     </>
   );
