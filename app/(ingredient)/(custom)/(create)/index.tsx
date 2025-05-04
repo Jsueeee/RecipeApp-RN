735ca33819119e9f0e0f8eea1f9384507a0e170d
Jsueeee <suyeon.aos@gmail.com>
2025-05-05 01:35:02 +0900

    feat: 커스텀 재료 만들기 화면에 재료 이름 및 카테고리 입력 기능 추가

--- app/(ingredient)/(custom)/(create)/index.tsx
+++ app/(ingredient)/(custom)/(create)/index.tsx
@@ -2,10 +2,15 @@ import { PressableScale } from "@/app/components/PressableScale";
 import SelectIngredientIconImage from "@/assets/images/img_select_ingredient_icon.svg";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { View } from "react-native";
+import { IngredientNameInput } from "./components/IngredientNameInput";
+import { IngredientCategorySelector } from "./components/IngredientCategorySelector";
 
 export default function CustomIngredientCreateScreen() {
+  const [ingredientName, setIngredientName] = useState("");
+  const [ingredientCategory, setIngredientCategory] = useState("");
+
   const onSelectIconPress = useCallback(() => {
     console.log("select icon");
   }, []);
@@ -24,6 +29,20 @@ export default function CustomIngredientCreateScreen() {
               style={{ alignSelf: "center" }}
             />
           </PressableScale>
+
+          <View className="h-3" />
+
+          <IngredientCategorySelector
+            selectedCategory={ingredientCategory}
+            onCategoryChanged={setIngredientCategory}
+          />
+
+          <View className="h-5" />
+
+          <IngredientNameInput
+            name={ingredientName}
+            onNameChanged={setIngredientName}
+          />
         </View>
       </ScreenLayout>
     </>
