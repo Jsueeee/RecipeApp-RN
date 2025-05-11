1eedde28392e60f64b6e72e45f1b0d606c4a7d84
Jsueeee <suyeon.aos@gmail.com>
2025-05-11 17:28:11 +0900

    feat: 구글 로그인 쿼리 키 추가

--- app/lib/query/keys.ts
+++ app/lib/query/keys.ts
@@ -3,6 +3,7 @@ import { RecipeSourceType } from "@/constants/RecipeSourceType";
 export const QUERY_KEYS = {
   AUTH: {
     KAKAO: ["auth", "kakao"] as const,
+    GOOGLE: ["auth", "google"] as const,
     AUTO_LOGIN: ["auth", "autoLogin"] as const,
   },
   FRIDGE: {
