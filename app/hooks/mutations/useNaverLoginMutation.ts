79366ad27f2697d72b2c0ea4762039b059e5f127
Jsueeee <suyeon.aos@gmail.com>
2025-05-18 22:14:06 +0900

    feat: 네이버 로그인 기능 추가 및 관련 훅 구현

---
+++ app/hooks/mutations/useNaverLoginMutation.ts
@@ -0,0 +1,53 @@
+import { apiClient } from "@/app/lib/api/client";
+import { QUERY_KEYS } from "@/app/lib/query/keys";
+import { authStorage } from "@/app/lib/storage/auth";
+import { LoginResponse } from "@/app/types/api/auth";
+import NaverLogin from "@react-native-seoul/naver-login";
+import { useMutation } from "@tanstack/react-query";
+
+export const useNaverLoginMutation = () => {
+  const naverLoginMutation = useMutation({
+    mutationKey: QUERY_KEYS.AUTH.NAVER,
+    mutationFn: async () => {
+      try {
+        const { failureResponse, successResponse } = await NaverLogin.login();
+
+        if (successResponse) {
+          const { accessToken } = successResponse;
+          if (!accessToken)
+            throw new Error("네이버 로그인에 실패했습니다. (accessToken 없음)");
+
+          const { data } = await apiClient.post<LoginResponse>(
+            "/users/naver-login",
+            {
+              accessToken,
+              fcmToken: "", // TODO: FCM 토큰 필요시 추가
+            }
+          );
+          return data;
+        } else if (failureResponse) {
+          throw new Error("네이버 로그인에 실패했습니다.", {
+            cause: failureResponse,
+          });
+        } else {
+          throw new Error("네이버 로그인에 실패했습니다.");
+        }
+      } catch (error: any) {
+        throw error;
+      }
+    },
+    onSuccess: async (data) => {
+      await authStorage.setTokens({
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+        userId: data.userId,
+      });
+    },
+  });
+
+  return {
+    naverLogin: naverLoginMutation.mutateAsync,
+    isLoading: naverLoginMutation.isPending,
+    error: naverLoginMutation.error,
+  };
+};
