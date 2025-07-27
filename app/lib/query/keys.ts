84ef155a879b397fc30d26ecfa673dc764c97e57
Jsueeee <suyeon.aos@gmail.com>
2025-07-27 21:13:49 +0900

    fix(query-keys): 쿼리 키 구조를 함수형으로 변경

--- app/lib/query/keys.ts
+++ app/lib/query/keys.ts
@@ -6,15 +6,18 @@ export const QUERY_KEYS = {
     VERSION: () => [...QUERY_KEYS.APP.ROOT, "version"] as const,
   },
   AUTH: {
-    KAKAO: ["auth", "kakao"] as const,
-    GOOGLE: ["auth", "google"] as const,
-    NAVER: ["auth", "naver"] as const,
-    AUTO_LOGIN: ["auth", "autoLogin"] as const,
+    ROOT: ["auth"] as const,
+    KAKAO: () => [...QUERY_KEYS.AUTH.ROOT, "kakao"] as const,
+    GOOGLE: () => [...QUERY_KEYS.AUTH.ROOT, "google"] as const,
+    NAVER: () => [...QUERY_KEYS.AUTH.ROOT, "naver"] as const,
+    AUTO_LOGIN: () => [...QUERY_KEYS.AUTH.ROOT, "autoLogin"] as const,
   },
   FRIDGE: {
-    FRIDGES: ["fridge", "fridges"] as const,
-    DETAIL: (fridgeId: number) => ["fridge", "detail", fridgeId] as const,
-    BASKET: ["fridge", "basket"] as const,
+    ROOT: ["fridge"] as const,
+    FRIDGES: () => [...QUERY_KEYS.FRIDGE.ROOT, "fridges"] as const,
+    DETAIL: (fridgeId: number) =>
+      [...QUERY_KEYS.FRIDGE.ROOT, "detail", fridgeId] as const,
+    BASKET: () => [...QUERY_KEYS.FRIDGE.ROOT, "basket"] as const,
   },
   RECIPE: {
     ROOT: ["recipe"] as const,
@@ -39,14 +42,19 @@ export const QUERY_KEYS = {
     MY_LIST: () => [...QUERY_KEYS.RECIPE.ROOT, "my-list"] as const,
   },
   SEARCH: {
-    POPULAR_KEYWORDS: ["search", "popularKeywords"] as const,
+    ROOT: ["search"] as const,
+    POPULAR_KEYWORDS: () =>
+      [...QUERY_KEYS.SEARCH.ROOT, "popularKeywords"] as const,
   },
   USER: {
     ROOT: ["user"] as const,
     INFO: () => [...QUERY_KEYS.USER.ROOT, "info"] as const,
   },
   INGREDIENT: {
-    PICK_LIST: (keyword?: string) => ["ingredients", "pick", keyword] as const,
-    MY: (keyword?: string) => ["ingredients", "my", keyword] as const,
+    ROOT: ["ingredients"] as const,
+    PICK_LIST: (keyword?: string) =>
+      [...QUERY_KEYS.INGREDIENT.ROOT, "pick", keyword] as const,
+    MY: (keyword?: string) =>
+      [...QUERY_KEYS.INGREDIENT.ROOT, "my", keyword] as const,
   },
 } as const;
