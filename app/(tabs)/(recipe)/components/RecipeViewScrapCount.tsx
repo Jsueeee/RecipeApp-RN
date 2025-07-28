7ae3e4200ddb0b4c0fd0e3e2f5d11917b431ef05
Jsueeee <suyeon.aos@gmail.com>
2025-07-29 00:34:39 +0900

    feat(recipe-view): 스크랩 상태에 따른 로티 애니메이션 커스텀

--- app/(tabs)/(recipe)/components/RecipeViewScrapCount.tsx
+++ app/(tabs)/(recipe)/components/RecipeViewScrapCount.tsx
@@ -1,7 +1,6 @@
-import HeartFillIcon from "@/assets/images/ic_heart_fill.svg";
-import HeartStrokeIcon from "@/assets/images/ic_heart_stroke.svg";
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import LottieView from "lottie-react-native";
+import React, { useEffect, useRef } from "react";
+import { TouchableOpacity } from "react-native";
 
 interface Props {
   viewCount?: number;
@@ -18,20 +17,70 @@ const RecipeViewScrapCount: React.FC<Props> = ({
 }) => {
   // TODO : 조회수 추후 추가 예정
 
-  return (
-    <TouchableOpacity
-      hitSlop={10}
-      onPress={onScrapClick}
-      className="flex-row items-center gap-[2px]"
-    >
-      {isScrapped ? (
-        <HeartFillIcon color="#F3734F" width={16} height={16} />
-      ) : (
-        <HeartStrokeIcon color="#9FADA6" width={16} height={16} />
-      )}
+  const animation = useRef<LottieView>(null);
 
-      <Text className="text-body4 text-text-normal">{scrapCount}</Text>
-    </TouchableOpacity>
+  const preValue = useRef(isScrapped);
+
+  useEffect(() => {
+    // isScrapped 상태가 변경되면 애니메이션 재생
+    if (isScrapped === preValue.current) return;
+
+    // // 초기 애니메이션 상태 지정
+    // if (preValue.current === null) {
+    //   preValue.current = isScrapped;
+
+    //   animation.current?.setState({
+    //     progress: isScrapped ? 1 : 0,
+    //   });
+
+    //   return;
+    // }
+
+    console.log("isScrapped", isScrapped);
+
+    if (isScrapped) {
+      animation.current?.reset();
+      animation.current?.play(500, 520);
+    } else {
+      animation.current?.reset();
+    }
+
+    preValue.current = isScrapped;
+  }, [isScrapped]);
+
+  return (
+    <>
+      <LottieView
+        ref={animation}
+        style={{
+          position: "absolute",
+          width: 70,
+          height: 70,
+          bottom: -20,
+          right: -20,
+        }}
+        loop={false}
+        source={require("@/assets/lottie/lottie_like_heart.json")}
+        progress={preValue.current ? 1 : 0}
+        // TODO : 컬러는 고민 좀 해보기
+        // colorFilters={[
+        //   {
+        //     keypath: "heart fill",
+        //     color: "#F3734F",
+        //   },
+        // ]}
+      />
+
+      {/* 로티 파일의 영역 때문에 따로 클릭 가능 영역을 지정 */}
+      <TouchableOpacity
+        hitSlop={10}
+        onPress={onScrapClick}
+        className="flex-row items-center gap-[2px] w-[30px] h-[30px] absolute bottom-0 right-0"
+      />
+
+      {/* TODO : 스크랩 카운트 추후 추가 예정 */}
+      {/* <Text className="text-body4 text-text-normal">{scrapCount}</Text> */}
+    </>
   );
 };
 
