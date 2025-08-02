fccb4f1e9125b04389ffa9fa868366c40ce05c4a
Jsueeee <suyeon.aos@gmail.com>
2025-08-03 02:38:11 +0900

    feat(recipe): 레시피 썸네일 컴포넌트에 이미지 선택 기능 추가

--- app/(recipe)/(create)/components/AddRecipeThumbnail.tsx
+++ app/(recipe)/(create)/components/AddRecipeThumbnail.tsx
@@ -1,14 +1,41 @@
 import IC_CAMERA from "@/assets/images/ic_camera.svg";
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Image, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export const AddRecipeThumbnail = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: false,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className="w-full max-w-[500px] aspect-square bg-gray-100 items-center justify-center"
+      onPress={pickImage}
     >
-      <IC_CAMERA width={40} height={40} />
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          className="w-full h-full"
+          resizeMode="cover"
+        />
+      ) : (
+        <IC_CAMERA width={40} height={40} />
+      )}
     </TouchableOpacity>
   );
 };
