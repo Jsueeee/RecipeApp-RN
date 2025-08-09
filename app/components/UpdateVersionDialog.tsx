069962387b597d1612f98b18b6b89a9ef4a089d1
Jsueeee <suyeon.aos@gmail.com>
2025-08-09 18:54:58 +0900

    fix(app): 강제 업데이트 다이얼로그 원버튼으로 변경

--- app/components/UpdateVersionDialog.tsx
+++ app/components/UpdateVersionDialog.tsx
@@ -1,7 +1,7 @@
 import { getStoreUrl } from "@/app/utils/VersionUtils";
-import { ChoiceDialog } from "@/components/ChoiceDialog";
+import { ConfirmDialog } from "@/components/ConfirmDialog";
 import i18n from "@/lib/i18n";
-import { BackHandler, Linking, Platform } from "react-native";
+import { Linking } from "react-native";
 
 interface Props {
   visible: boolean;
@@ -17,21 +17,13 @@ export const UpdateVersionDialog = ({ visible }: Props) => {
     }
   };
 
-  const onCloseApp = () => {
-    if (Platform.OS === "android") {
-      BackHandler.exitApp();
-    }
-  };
-
   return (
-    <ChoiceDialog
+    <ConfirmDialog
       visible={visible}
       title={i18n.t("app.version_title")}
       message={i18n.t("app.version_message")}
       confirmText={i18n.t("app.version_confirm")}
-      cancelText={i18n.t("app.version_cancel")}
       onConfirm={handleUpdatePress}
-      onCancel={onCloseApp}
     />
   );
 };
