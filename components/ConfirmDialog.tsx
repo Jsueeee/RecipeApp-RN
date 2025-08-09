f256aa36438acc3bd45ec6d2aa30533d2723fa9e
Jsueeee <suyeon.aos@gmail.com>
2025-08-09 17:41:37 +0900

    feat(app): [REC-51] 확인 버튼이 있는 다이얼로그 컴포넌트 추가

---
+++ components/ConfirmDialog.tsx
@@ -0,0 +1,55 @@
+import { CTAButton } from "@/components/CTAButton";
+import { Modal, Pressable, Text, View } from "react-native";
+
+interface Props {
+  visible: boolean;
+  title: string;
+  message?: string;
+  confirmText?: string;
+  isConfirmLoading?: boolean;
+  onConfirm: () => void;
+}
+
+/**
+ * 확인 버튼 한개만 있는 다이얼로그
+ */
+export const ConfirmDialog = ({
+  visible,
+  title,
+  message,
+  confirmText,
+  isConfirmLoading = false,
+  onConfirm,
+}: Props) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      statusBarTranslucent={true}
+      animationType="fade"
+    >
+      <Pressable
+        className="flex-1 bg-material-dimmer justify-center items-center"
+        onPress={onConfirm}
+      >
+        <Pressable
+          className="bg-white rounded-[16px] p-4 w-[80%] max-w-[400px]"
+          onPress={(e) => e.stopPropagation()}
+        >
+          <Text className="text-title4 text-text-normal mt-1">{title}</Text>
+
+          {message && <Text className="text-body3 mt-3">{message}</Text>}
+
+          <View className="flex-row gap-2 mt-6">
+            <CTAButton
+              buttonLabel={confirmText ?? ""}
+              isLoading={isConfirmLoading}
+              onPress={onConfirm}
+              className="flex-1"
+            />
+          </View>
+        </Pressable>
+      </Pressable>
+    </Modal>
+  );
+};
