58548ccbd3d1edbe854d8ab2d17801b17647a0d5
Jsueeee <suyeon.aos@gmail.com>
2025-08-21 01:01:50 +0900

    fix(modal): 레시피 상세 화면에 있는 모달 컴포넌트들에 UnscaledModalRoot 컴포넌트 추가하여 모달 스케일 문제 해결

--- app/(recipe)/(detail)/components/DeleteRecipeDialog.tsx
+++ app/(recipe)/(detail)/components/DeleteRecipeDialog.tsx
@@ -1,4 +1,5 @@
 import { ChoiceDialog } from "@/components/ChoiceDialog";
+import { UnscaledModalRoot } from "@/components/UnscaledModalRoot";
 import i18n from "@/lib/i18n";
 import React from "react";
 
@@ -10,14 +11,16 @@ interface Props {
 
 export const DeleteRecipeDialog = ({ visible, onClose, onConfirm }: Props) => {
   return (
-    <ChoiceDialog
-      visible={visible}
-      title={i18n.t("recipe_detail.delete_dialog_title")}
-      message={i18n.t("recipe_detail.delete_dialog_desc")}
-      confirmText={i18n.t("recipe_detail.delete_dialog_confirm")}
-      cancelText={i18n.t("recipe_detail.delete_dialog_cancel")}
-      onCancel={onClose}
-      onConfirm={onConfirm}
-    />
+    <UnscaledModalRoot>
+      <ChoiceDialog
+        visible={visible}
+        title={i18n.t("recipe_detail.delete_dialog_title")}
+        message={i18n.t("recipe_detail.delete_dialog_desc")}
+        confirmText={i18n.t("recipe_detail.delete_dialog_confirm")}
+        cancelText={i18n.t("recipe_detail.delete_dialog_cancel")}
+        onCancel={onClose}
+        onConfirm={onConfirm}
+      />
+    </UnscaledModalRoot>
   );
 };
