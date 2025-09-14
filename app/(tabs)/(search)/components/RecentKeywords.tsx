33fb6cca40ee7c424f72d987130cd65634c13ea1
Jsueeee <suyeon.aos@gmail.com>
2025-09-14 19:21:03 +0900

    chore(search): 검색 화면 컨텐츠 부족으로 인해 스크롤뷰 제거

--- app/(tabs)/(search)/components/RecentKeywords.tsx
+++ app/(tabs)/(search)/components/RecentKeywords.tsx
@@ -30,7 +30,8 @@ export function RecentKeywords({
         </PressableScale>
       </View>
 
-      <ScrollView
+      {/* 검색화면 컨텐츠가 많아지면 다시 스크롤로 바꾸기 */}
+      {/* <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
         className="w-full mt-[14px]"
@@ -50,7 +51,23 @@ export function RecentKeywords({
             </View>
           </PressableScale>
         ))}
-      </ScrollView>
+      </ScrollView> */}
+      <View className="mt-3.5 flex-row flex-wrap gap-x-1.5 gap-y-2 px-4">
+        {keywords.map((keyword, index) => (
+          <PressableScale
+            key={keyword + index}
+            onPress={() => onKeywordPress(keyword)}
+          >
+            <View className="flex-row items-center gap-1 bg-fill-subtle rounded-[12px] px-3 py-1.5">
+              <Text className="text-body3 text-text-normal">{keyword}</Text>
+
+              <Pressable hitSlop={6} onPress={() => onRemovePress(keyword)}>
+                <CloseIcon width={16} height={16} color="#9FADA6" />
+              </Pressable>
+            </View>
+          </PressableScale>
+        ))}
+      </View>
     </View>
   );
 }
