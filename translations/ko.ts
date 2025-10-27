81226ab4dba31649bca66a75a42d6d5e275abf7a
Jsueeee <suyeon.aos@gmail.com>
2025-10-27 23:47:40 +0900

    chore(login): 로그인 버튼 ko 수정

--- translations/ko.ts
+++ translations/ko.ts
@@ -34,10 +34,10 @@ export default {
   },
   login: {
     title: "먹는 거에 진심인\n우리들의 공간",
-    with_KAKAO: "카카오로 로그인",
-    with_NAVER: "네이버로 로그인",
-    with_GOOGLE: "구글로 로그인",
-    with_APPLE: "애플로 로그인",
+    with_KAKAO: "Kakao로 로그인",
+    with_NAVER: "Naver로 로그인",
+    with_GOOGLE: "Google로 로그인",
+    with_APPLE: "Apple로 로그인",
   },
   category: {
     youtube: "유튜브",
