6c4f377044bbf5198c9fa8ca6334786b0b534bac
Jsueeee <suyeon.aos@gmail.com>
2025-11-02 19:23:26 +0900

    feat(common): 로딩스크린에 딤 옵션 추가

--- components/DotLoadingScreen.tsx
+++ components/DotLoadingScreen.tsx
@@ -3,13 +3,16 @@ import { View } from "react-native";
 import { TealDotLoading } from "./DotLoading";
 
 interface Props {
+  isShowDim?: boolean;
   className?: string;
 }
 
-export const DotLoadingScreen = ({ className }: Props) => {
+export const DotLoadingScreen = ({ isShowDim = false, className }: Props) => {
   return (
     <View
-      className={`absolute left-0 right-0 top-0 bottom-0 items-center justify-center ${className} z-10`}
+      className={`absolute left-0 right-0 top-0 bottom-0 items-center justify-center ${className} z-10 ${
+        isShowDim ? "bg-[#0000004D]" : ""
+      }`}
     >
       <TealDotLoading />
     </View>
