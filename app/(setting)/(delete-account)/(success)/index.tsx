c6236fbcb5da35bf16d52f8e9dc8126f221be94f
Jsueeee <suyeon.aos@gmail.com>
2025-11-02 16:55:06 +0900

    feat(delete-account): 탈퇴 성공화면 ui 추가

--- app/(setting)/(delete-account)/(success)/index.tsx
+++ app/(setting)/(delete-account)/(success)/index.tsx
@@ -1,11 +1,35 @@
+import IC_TOMATO from "@/assets/images/ic_tomato.svg";
+import { CTAButton } from "@/components/CTAButton";
 import { ScreenLayout } from "@/components/layout/ScreenLayout";
 import i18n from "@/lib/i18n";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { router } from "expo-router";
 
 export default function DeleteAccountSuccessScreen() {
+  const onCTAButtonPress = () => {
+    router.dismissAll();
+    router.replace("/(auth)");
+  };
+
   return (
     <ScreenLayout isShowHeader={false}>
-      <Text>{i18n.t("delete_account_success.title")}</Text>
+      <View className="items-center justify-center flex-1">
+        <IC_TOMATO width={72} />
+
+        <Text className="text-title3 text-text-strong mt-4">
+          {i18n.t("delete_account_success.title")}
+        </Text>
+
+        <Text className="text-body2 text-text-alternative mt-2 text-center">
+          {i18n.t("delete_account_success.message")}
+        </Text>
+      </View>
+
+      <CTAButton
+        buttonLabel={i18n.t("delete_account_success.cta")}
+        onPress={onCTAButtonPress}
+        className="mx-4 mb-[22px]"
+      />
     </ScreenLayout>
   );
 }
