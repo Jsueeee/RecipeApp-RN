bff9f3e9eba377e5d83e95fc2b2b8b6c7b6ef678
Jsueeee <suyeon.aos@gmail.com>
2025-11-02 22:21:47 +0900

    feat(login): 옵션 로그인 바텀시트에 이메일 보내기 버튼 추가

--- app/(auth)/components/OptionalLoginBottomSheet.tsx
+++ app/(auth)/components/OptionalLoginBottomSheet.tsx
@@ -1,10 +1,13 @@
+import RightArrowIcon from "@/assets/images/ic_arrow_right.svg";
 import DefaultBottomSheetModal from "@/components/DefaultBottomSheetModal";
 import i18n from "@/lib/i18n";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import React from "react";
-import { Platform, View } from "react-native";
+import { Linking, Platform, Text, View } from "react-native";
 import { useAuth } from "../hooks/useAuth";
 import { DefaultLoginButton, LoginMethod } from "./LoginButton";
+import { PressableScale } from "@/app/components/PressableScale";
+import { router } from "expo-router";
 
 interface Props {
   bottomSheetModalRef: React.RefObject<BottomSheetModal | null>;
@@ -30,6 +33,19 @@ export const OptionalLoginBottomSheet = ({
     }
   };
 
+  const onPressCSEmail = () => {
+    const email = "recipestorage2021@gmail.com";
+    const subject = "[레시피 저장소] 문의";
+    const body = "여기에 내용을 입력해 주세요.";
+    const url = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+
+    Linking.openURL(url).catch((err) =>
+      console.error("이메일 열기 실패:", err)
+    );
+  };
+
   return (
     <DefaultBottomSheetModal
       bottomSheetModalRef={bottomSheetModalRef}
@@ -50,6 +66,26 @@ export const OptionalLoginBottomSheet = ({
           isOptional={true}
           onClick={() => onPressLogin(LoginMethod.NAVER)}
         />
+
+        <PressableScale
+          onPress={onPressCSEmail}
+          className="self-start self-center my-4"
+          style={{
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 8,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          pressedStyle={{ backgroundColor: "#0000001A" }}
+        >
+          <Text className="text-body2 text-text-normal">
+            {i18n.t("setting.CSEmail")}
+          </Text>
+
+          <RightArrowIcon width={20} height={20} color="#3F4542" />
+        </PressableScale>
       </View>
     </DefaultBottomSheetModal>
   );
