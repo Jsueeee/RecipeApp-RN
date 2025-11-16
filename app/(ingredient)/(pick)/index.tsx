0fd8d0644fe7709467926d8f40a8e9850dcc489d
Jsueeee <suyeon.aos@gmail.com>
2025-11-17 01:10:10 +0900

    fix(basket): 바구니 화면으로 이동할 때 dismissAll() 이 필요없다

--- app/(ingredient)/(pick)/index.tsx
+++ app/(ingredient)/(pick)/index.tsx
@@ -42,7 +42,6 @@ export default function IngredientPickScreen() {
     {
       onSuccess: () => {
         setSelectedIngredients([]);
-        router.dismissAll();
         router.push("/(fridge)/(basket)");
       },
       onError: (error) => {
